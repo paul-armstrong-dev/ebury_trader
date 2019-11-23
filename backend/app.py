bedf64abff1c331f06de69d309b187b844d56925
Paul Armstrong <paul.armstrong211@gmail.com>
2019-11-23 10:58:41 +0200

    Renamed incorrect import after sonarclouding; Renamed DB name in init; Minor consistency change in compose.yml

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from api.api import api
 from api.config import Config
 from api.forex_models import db
-from api.model_handler import DB_Utils
+from api.model_handler import DbUtils
 from flask import Flask
 from flask_cors import CORS
 
@@ -11,17 +11,18 @@ def create_app(config):
     CORS(app)
     app.config.from_object(config)
     register_extensions(app)
+
     return app
 
 
 def register_extensions(app):
     api.init_app(app)
     db.init_app(app)
+    DbUtils.recreate_db(Config.SQLALCHEMY_DATABASE_URI)
 
 
 # Run the application
 if __name__ == '__main__':
     app = create_app(Config)
-    DB_Utils.recreate_db(Config.SQLALCHEMY_DATABASE_URI)
-    DB_Utils.add_test_trade(Config.SQLALCHEMY_DATABASE_URI)
+    # DbUtils.add_test_trade(Config.SQLALCHEMY_DATABASE_URI)
     app.run(host='0.0.0.0', port=80, threaded=True)
