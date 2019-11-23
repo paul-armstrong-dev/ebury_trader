87ee433925fa45133ffbc41319630eb86c9b2fac
Paul Armstrong <paul.armstrong211@gmail.com>
2019-11-23 11:39:36 +0200

    Css error on build corrected

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,14 +2,10 @@ import React from 'react';
 
 import {Col, Container, Nav, Navbar, Row} from 'reactstrap';
 
-const AVATAR = 'https://www.gravatar.com/avatar/429e504af19fc3e1cfa5c4326ef3394c?s=240&d=mm&r=pg';
-
-
-
 const Header = () => (
     <header>
         <Navbar fixed="top" color="light" light expand="xs" className="border-bottom border-gray bg-white"
-                style={{height: 80}}>
+                style={{height: 60}}>
 
             <Container>
                 <Row noGutters className="position-relative w-100 align-items-center">
